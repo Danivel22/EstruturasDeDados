208e5969ab44a805aaf67809f034f1962c531794
Danivel Doubhglas <66448626+Danivel22@users.noreply.github.com>
2025-03-17 23:41:04 -0300

    Update and rename texto.txt to texto.py

---
+++ Conceito Lista Encadeada/texto.py
@@ -0,0 +1,8 @@
+""" https://www.youtube.com/watch?v=RW0oD2L_tSg 
+"""
+
+# Definição da Classe Node (Nó)
+#Cada nó da lista contém:
+
+#Um valor armazenado (self.valor).
+#Um ponteiro para o próximo nó (self.proximo), que inicialmente é None.
